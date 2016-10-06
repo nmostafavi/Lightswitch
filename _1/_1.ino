8d8570244698235b5b499e03f1b44159578b4040
Neema Mostafavi <github@neema.me>
2016-10-06 00:01:22 -0700

    Changing my approach. No more heuristics.

--- _1/_1.ino
+++ _1/_1.ino
@@ -8,12 +8,10 @@ const int led_pin = 13;
 const int threshold_high = 500;  // any value greater than this is considered HIGH
 const int threshold_low = 200;  // any value less than this is considered LOW
 
-unsigned long idle_threshold = 20000;
-
 void setup() {
   Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
-  // Start recording
+  // Start recording immediately.
   bool previous_state = LOW;
   unsigned long last_state_change_time = 0;
   int count = 0;
@@ -22,7 +20,8 @@ void setup() {
   for (int i = 0; i < num_data_points; i++) {
     data[i] = 0;
   }
-  bool started_recording = false;
+  digitalWrite(led_pin, LOW);
+  Serial.println("Started recording.");
   while (true) {
     int val = analogRead(rf_receive_pin);
     bool current_state = previous_state;
@@ -31,41 +30,29 @@ void setup() {
     } else if (val < threshold_low) {
       current_state = LOW;
     }
-    if (started_recording == false && previous_state == HIGH && current_state == LOW) {
-      // Start recording data, starting with the duration of the first HIGH state
-      started_recording = true;
-      Serial.println("Started recording.");
+    unsigned long current_time = micros();
+    unsigned long time_since_last_state_change = current_time - last_state_change_time;
+    if (current_time < last_state_change_time) {
+      // The current_time value overflowed. Account for the rollover.
+      time_since_last_state_change = (UNSIGNED_LONG_MAX - last_state_change_time) + current_time;
     }
-    if (started_recording == true) {
-      unsigned long current_time = micros();
-      unsigned long time_since_last_state_change = current_time - last_state_change_time;
-      if (current_time < last_state_change_time) {
-        // The current_time value overflowed. Account for the rollover.
-        time_since_last_state_change = (UNSIGNED_LONG_MAX - last_state_change_time) + current_time;
-      }
-      if (current_state == LOW && time_since_last_state_change > idle_threshold) {
-        // Received LOW state for longer than the specified threshold.
-        // No new data appears to be streaming in.
-        Serial.println("Detected idle state.");
+    if (current_state != previous_state) {
+      last_state_change_time = current_time;
+      if (count < num_data_points) {
+        data[count] = time_since_last_state_change;
+      } else {
+        Serial.println("Finished recording.");
         break;
       }
-      if (current_state != previous_state) {
-        if (count < num_data_points) {
-          data[count] = time_since_last_state_change;
-        } else {
-          // Recording memory is full.
-          Serial.println("Ran out of storage space.");
-          break;
-        }
-        last_state_change_time = current_time;
-        count++;
-      }
+      count++;
     }
     previous_state = current_state;
   }
   digitalWrite(led_pin, HIGH);
   // Dump the received data
-  for (int i=0; i<=count; i++) {
+  Serial.print(count);
+  Serial.println(" data points.");
+  for (int i = 0; i < num_data_points; i++) {
     Serial.println(data[i]);
   }
 }
