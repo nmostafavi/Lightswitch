0fcb2a99fbf3d7b267d4f348a563c4fec3e73bb9
Neema Mostafavi <github@neema.me>
2016-10-05 20:37:15 -0700

    Added bicolor LED sfunctions

--- lightswitch/lightswitch.ino
+++ lightswitch/lightswitch.ino
@@ -3,12 +3,16 @@ const int button_pin = 2;
 const int speaker_pin = 8;
 const int rf_receive_pin = A0;
 const int rf_transmit_pin = 4;
+const int bicolor_led_anode_pin = 7;
+const int bicolor_led_cathode_pin = 6;
 
 void setup() {
   Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
   pinMode(button_pin, INPUT_PULLUP);
   pinMode(rf_transmit_pin, OUTPUT);
+  pinMode(bicolor_led_anode_pin, OUTPUT);
+  pinMode(bicolor_led_cathode_pin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(button_pin), button_press, FALLING);
 }
 
@@ -29,5 +33,16 @@ void chirp() {
   delay(duration + pause);
   tone(speaker_pin, frequency, duration);
   digitalWrite(led_pin, LOW);
+}
+
+
+void led_red() {
+  digitalWrite(bicolor_led_anode_pin, HIGH);
+  digitalWrite(bicolor_led_cathode_pin, LOW);
+}
+
+void led_green() {
+  digitalWrite(bicolor_led_anode_pin, LOW);
+  digitalWrite(bicolor_led_cathode_pin, HIGH);
 }
 
