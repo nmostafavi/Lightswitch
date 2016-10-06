a90235882d6a3586f1bea2778f6c65f05c4fc144
Neema Mostafavi <github@neema.me>
2016-10-05 20:13:00 -0700

    cleanup

--- lightswitch/lightswitch.ino
+++ lightswitch/lightswitch.ino
@@ -3,32 +3,31 @@ const int button_pin = 2;
 const int speaker_pin = 8;
 const int rf_receive_pin = A0;
 const int rf_transmit_pin = 4;
-volatile bool button_state = LOW;
 
 void setup() {
   Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
   pinMode(button_pin, INPUT_PULLUP);
   pinMode(rf_transmit_pin, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(button_pin), button_press, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(button_pin), button_press, FALLING);
 }
 
 void loop() {
 }
 
 void button_press() {
-  bool button_state = !digitalRead(button_pin);
-  if (button_state == HIGH) {
-    chirp();
-  }
+  // can attempt to de-bounce button input here, if needed.
+  chirp();
 }
 
 void chirp() {
   unsigned int frequency = 31;  //2100;  // Hz
   unsigned int duration = 100;  // ms
   unsigned int pause = 10;  // ms
+  digitalWrite(led_pin, HIGH);
   tone(speaker_pin, frequency, duration);
   delay(duration + pause);
   tone(speaker_pin, frequency, duration);
+  digitalWrite(led_pin, LOW);
 }
 
