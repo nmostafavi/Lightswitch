ce085bd1803a5f0282ed093220ab53143a046f4f
Neema Mostafavi <github@neema.me>
2016-11-05 01:59:31 -0700

    Added long button press handling to the debounced button program

--- test_debounce_button/test_debounce_button.ino
+++ test_debounce_button/test_debounce_button.ino
@@ -3,8 +3,13 @@
 const int led_pin = 13;
 const int button_pin = 2;
 
-// The debounced button state. For an INPUT_PULLUP pin, the default state is HIGH.
-bool button_state = HIGH;
+// The filtered (i.e. debounced) button state. For an INPUT_PULLUP pin, the default state is HIGH.
+bool filtered_button_state = HIGH;
+// The last time the filtered (i.e. debounced) button state has changed.
+unsigned long last_filtered_button_state_change_time = 0;
+// How many milliseconds the button has to be held down to register as a "long press". This can be used to initiate a special mode in the program.
+unsigned long long_press_duration = 1200;
+
 // The unfiltered last state of the button.
 bool last_unfiltered_button_state = HIGH;
 // The last time the button's unfiltered state has changed.
@@ -19,6 +24,7 @@ void setup() {
 }
 
 void loop() {
+  // Perform duration-threshold debouncing of button presses
   bool state = digitalRead(button_pin);
   unsigned long now = millis();
   if (state != last_unfiltered_button_state) {
@@ -26,13 +32,20 @@ void loop() {
     last_unfiltered_button_state = state;
   }
   if ((now - last_unfiltered_button_state_change_time) > debounce_threshold) {
-    if (state != button_state) {
-      button_state = state;
+    if (state != filtered_button_state) {
+      filtered_button_state = state;
+      last_filtered_button_state_change_time = now;
       if (state == HIGH) {
         Serial.println("HIGH"); 
       } else {
         Serial.println("LOW");
       }
+    }
+  }
+  // Detect and respond to long button presses using the debounced button input.
+  if (filtered_button_state == LOW) {
+    if ((now - last_filtered_button_state_change_time) > long_press_duration) {
+      Serial.println("**** LONG PRESS ****");
     }
   }
 }
