dd1cd4aec92c4c7307aa33ddf71c34a3e02818b8
Neema Mostafavi <github@neema.me>
2016-11-05 03:24:15 -0700

    comments and structure

--- lightswitch/lightswitch.ino
+++ lightswitch/lightswitch.ino
@@ -2,18 +2,21 @@
 #include "LED.h"
 #include "RF.h"
 
-const int led_pin = 13;
-const int button_pin = 2;
+// Set up piezo speaker
 const int speaker_pin = 8;
-const int rf_receive_pin = A0;
-const int rf_transmit_pin = 4;
+Piezo piezo(speaker_pin);
+// Set up LEDs
 const int bicolor_led_anode_pin = 7;
 const int bicolor_led_cathode_pin = 6;
-volatile bool button_pressed = false;
-
-Piezo piezo(speaker_pin);
 LED led(bicolor_led_anode_pin, bicolor_led_cathode_pin);
+// Set up RF receiver/transmitter and recording to EEPROM
+const int rf_receive_pin = A0;
+const int rf_transmit_pin = 4;
 RF rf(rf_receive_pin, rf_transmit_pin);
+// Set up other
+const int led_pin = 13;
+const int button_pin = 2;
+volatile bool button_pressed = false;
 
 void setup() {
   Serial.begin(9600);
