19c5cc21c9b45d7d64b2a792f90f18d859958016
Neema Mostafavi <github@neema.me>
2016-12-24 22:37:05 -0800

    stashing some old debug test stuff I forgot about

--- lightswitch/src/main.cpp
+++ lightswitch/src/main.cpp
@@ -25,7 +25,7 @@ const int led_pin = 13;
 bool is_recording = false;
 
 void setup() {
-  Serial.begin(9600);
+  // Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
   Serial.println("Ready.");
 }
@@ -35,27 +35,30 @@ void loop() {
   if (button.long_pressed()) {
     Serial.println("Beginning recording of the \"on\" signal.");
     // Beep and set LED to red
-    // piezo.tone_down();
+    piezo.tone_down();
     led.red();
     // Begin recording the "on" signal.
-    delay(2000);  // rf.record(true);
+    rf.record(true);
     // When finished, confirm with a flat beep.
-    // piezo.tone_mid();
+    piezo.tone_mid();
     // Wait until next button press.
     Serial.println("Finished recording \"on\" signal. Waiting for button press before recording the \"off\" signal.");
     is_recording = true;
   } else if (button.pressed() && is_recording == true) {
     // Begin recording the "off" signal
-    delay(2000);  // rf.record(false);
+    rf.record(false);
     // When finished, confirm with an ascending beep and set the LED to green.
-    // piezo.tone_up();
+    piezo.tone_up();
     led.green();
     Serial.println("Finished recording the \"off\" signal.");
     Serial.println("Ready.");
     is_recording = false;
   } else if (button.pressed() && is_recording == false) {
     // Send a test on/off signal
-
+    rf.playback(true);
+    delay(1000);
+    rf.playback(false);
+    led.off();
   } else {
     // Monitoring mode: Watch for changes to the input pin, then transmit the
     // corresponding RF signal.
