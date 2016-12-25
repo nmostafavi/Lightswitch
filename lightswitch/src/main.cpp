535a4e2bc0bd4a73963470751700922bc0338c0c
Neema Mostafavi <github@neema.me>
2016-12-25 00:58:33 -0800

    successfully tested transmission of rf codes to control an outlet

--- lightswitch/src/main.cpp
+++ lightswitch/src/main.cpp
@@ -2,8 +2,8 @@
 #include <RCSwitch.h>
 
 // Set up RF receiver/transmitter
-const int rf_interrupt_pin = 0;
-const int rf_transmit_pin = 4;
+const int rf_interrupt = digitalPinToInterrupt(2);
+const int rf_transmit_pin = 3;
 RCSwitch rf = RCSwitch();
 // Set up other
 const int led_pin = 13;
@@ -11,24 +11,38 @@ const int led_pin = 13;
 void setup() {
   Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
-  rf.enableReceive(rf_interrupt_pin);
-  Serial.println("Ready.");
+  // rf.enableReceive(rf_interrupt);
+  // Serial.println("Ready to receive.");
+
+  rf.enableTransmit(rf_transmit_pin);
+  rf.setProtocol(1);
+  rf.setPulseLength(190);
+  Serial.println("Ready to transmit.");
 }
 
 void loop() {
-  if (rf.available()) {
-    int value = rf.getReceivedValue();
-    if (value == 0) {
-      Serial.println("Unknown encoding.");
-    } else {
-      Serial.print("Received ");
-      Serial.print(rf.getReceivedValue());
-      Serial.print(" / ");
-      Serial.print(rf.getReceivedBitlength());
-      Serial.print("bit ");
-      Serial.print("Protocol: ");
-      Serial.println(rf.getReceivedProtocol());
-    }
-    rf.resetAvailable();
-  }
+
+  rf.send(5510451, 24);
+  delay(1000);
+  rf.send(5510460, 24);
+  delay(1000);
+  
+  // if (rf.available()) {
+  //   int value = rf.getReceivedValue();
+  //   if (value == 0) {
+  //     Serial.println("Unknown encoding.");
+  //   } else {
+  //     Serial.print("Received ");
+  //     Serial.print(rf.getReceivedValue());
+  //     Serial.print(" / ");
+  //     Serial.print(rf.getReceivedBitlength());
+  //     Serial.print("bit ");
+  //     Serial.print(" PulseLength: ");
+  //     Serial.print(rf.getReceivedDelay());
+  //     Serial.print(" microseconds");
+  //     Serial.print(" Protocol: ");
+  //     Serial.println(rf.getReceivedProtocol());
+  //   }
+  //   rf.resetAvailable();
+  // }
 }
