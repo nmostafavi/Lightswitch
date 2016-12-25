6c915926acbff66260e1df3e73559bb7d2df2134
Neema Mostafavi <github@neema.me>
2016-12-24 23:27:18 -0800

    Successfully tested the rcswitch library in receiving the remote control codes for my remote light switch system.

--- lightswitch/src/main.cpp
+++ lightswitch/src/main.cpp
@@ -1,45 +1,34 @@
 #include <Arduino.h>
-#include <EEPROM.h>
-#include <Piezo.h>
-#include <LED.h>
-#include <RF.h>
-#include <Button.h>
+#include <RCSwitch.h>
 
-// Set up piezo speaker
-const int speaker_pin = 8;
-Piezo piezo(speaker_pin);
-// Set up LEDs
-const int bicolor_led_anode_pin = 7;
-const int bicolor_led_cathode_pin = 6;
-LED led(bicolor_led_anode_pin, bicolor_led_cathode_pin);
-// Set up RF receiver/transmitter and recording to EEPROM
-const int rf_receive_pin = A0;
+// Set up RF receiver/transmitter
+const int rf_interrupt_pin = 0;
 const int rf_transmit_pin = 4;
-RF rf(rf_receive_pin, rf_transmit_pin);
-// Set up button
-const int button_pin = 2;
-Button button(button_pin);
+RCSwitch rf = RCSwitch();
 // Set up other
 const int led_pin = 13;
 
-bool is_recording = false;
-
 void setup() {
-  // Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
+  rf.enableReceive(rf_interrupt_pin);
   Serial.println("Ready.");
 }
 
 void loop() {
-  button.update();
-  if (button.pressed()) {
-    led.red();
-    piezo.tone_up();
-    delay(100);
-    // rf.record(true);
-    rf.playback(true);
-    delay(100);
-    piezo.tone_down();
-    led.off();
+  if (rf.available()) {
+    int value = rf.getReceivedValue();
+    if (value == 0) {
+      Serial.println("Unknown encoding.");
+    } else {
+      Serial.print("Received ");
+      Serial.print(rf.getReceivedValue());
+      Serial.print(" / ");
+      Serial.print(rf.getReceivedBitlength());
+      Serial.print("bit ");
+      Serial.print("Protocol: ");
+      Serial.println(rf.getReceivedProtocol());
+    }
+    rf.resetAvailable();
   }
 }
